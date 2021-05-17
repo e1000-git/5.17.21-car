c070df10467a58a90feddd8e6e7951a99428a24f
haceli6 <haceli@list.ru>
2021-05-17 13:06:21 +0400

    5.17.21

---
+++ 5.17.21/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _5._17._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car("BMW", "M5", 50, 80, 15);
+            string input;
+            do
+            {
+                Console.WriteLine("please select number");
+                Console.WriteLine("1.Go");
+                Console.WriteLine("2.Top Up");
+                Console.WriteLine("2.Top Up");
+
+                Console.WriteLine("3.Stop");
+                Console.WriteLine("4.Exit");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        car1.Go();
+                        break;
+                    case "2":
+                        car1.TopUp();
+                        break;
+                    case "3":
+                        Console.WriteLine("Stop");
+                        break;
+                    case "4":
+                        Console.WriteLine("Yaxsi yol");
+                        break;
+
+                    default:
+                        Console.WriteLine("Please selecet number from 1 to 4");
+                        break;
+                }
+            } while (input !="4");
+
+
+
+        }
+    }
+    
+}
---
+++ 5.17.21/car.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._17._21
+{
+    class car
+    {
+        public string Marka;
+        public string Model;
+        public decimal CurrentFuel;
+        public decimal MaxFuel;
+        public decimal FuelEff;
+
+        public car(string marka, string model, decimal currentFuel, decimal maxFuel, decimal fuelEff)
+        {
+            Marka = marka;
+            Model = model;
+            CurrentFuel = currentFuel;
+            MaxFuel = maxFuel;
+            FuelEff = fuelEff;
+        }
+        public void Go(){
+
+        Geri:
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Nece km getmek isteyirsiz?");
+            string neededKm = Console.ReadLine();
+            try
+            {
+                decimal neededKmDec = Convert.ToDecimal(neededKm);
+                if(CurrentFuel>= neededKmDec * FuelEff / 100)
+                {
+                    CurrentFuel -= neededKmDec * FuelEff / 100;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Siz {neededKmDec}km yol getdiniz. Masinda {CurrentFuel}l benzin qaldi");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Zehmet olmasa benzin doldurun. Benzin kifayet etmedi");
+                }
+            }
+            catch (Exception)
+            {
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please, write numeric number");
+                goto Geri;
+            }
+        }
+        public void TopUp()
+        {
+            Geri:
+            Console.WriteLine("Nece litr benzin doldurmaq isteyirsiz?");
+            string neededFuel=Console.ReadLine();
+            try
+            {
+                decimal neededFuelDec = Convert.ToDecimal(neededFuel);
+                if (MaxFuel >= neededFuelDec + CurrentFuel)
+                {
+                    CurrentFuel += neededFuelDec;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Siz {neededFuelDec}l benzin doldurdunuz. Masinda {CurrentFuel}l benzin var.");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Maximum tutumu kecdiniz.");
+                }
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please, write numeric number");
+                goto Geri;
+            }
+        }
+    }
+}
